6a30c197092f8438e4202acfeacc50fe221ce811
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-07-05 23:20:59 +0300

    feedback done

--- src/Components/Feedback/Feedback.jsx
+++ src/Components/Feedback/Feedback.jsx
@@ -1,8 +1,14 @@
-const Feedback = ({ reviewsType }) => {
+import s from "./Feedback.module.css";
+
+const Feedback = ({ totalFeedback, good, neutral, bad, positiveFeedback }) => {
   return (
-    <div>
-      <p>{reviewsType}</p>
-    </div>
+    <ul className={s.list}>
+      <li className={s.item}>Good: {good}</li>
+      <li className={s.item}>Neutral: {neutral}</li>
+      <li className={s.item}>Bad: {bad}</li>
+      <li className={s.item}>Total: {totalFeedback}</li>
+      <li className={s.item}>Positive: {positiveFeedback}%</li>
+    </ul>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
